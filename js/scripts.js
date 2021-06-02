2b3b0f9980d99e6b6d0bedd517dfb8aa561e7a2e
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-06-02 11:25:29 -0700

    Showcase the use of jQuery to make a form letter.
    Co-authored-by: Erika Debelis <erika.debelis@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const nameInput = $("input#name").val();
+
+    $(".name").text(nameInput);
+
+    $("#letter").show();
+
+    
+  });
+});
